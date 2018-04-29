b020cb51d59c386e8503ce3c124f65c1783d74fd
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-04-29 10:59:44 -0700

    Add cors

--- random-word-server/server.js
+++ random-word-server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
+
+// CORS for react app, assuming port 3000
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
 
 // read words from json file
 const fileContents = fs.readFileSync('./five-letter-words.json', 'utf-8');
