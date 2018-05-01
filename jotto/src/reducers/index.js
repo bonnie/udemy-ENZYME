11c0d414e0060b446aafae1cbcde53acc056bbde
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-05-01 08:40:42 -0700

    Set up redux

---
+++ jotto/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import success from './successReducer';
+
+export default combineReducers({
+  success,
+});
---
+++ jotto/src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+export default (state, action) => {
+  return null;
+}
