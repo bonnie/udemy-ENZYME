ed0f91aef12463b7ee4f0dda9cb8a5ea5b5de22f
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-04-30 17:18:49 -0700

    Tests and code for non-zero guessed words

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -9,6 +9,26 @@ const GuessedWords = (props) => {
         Try to guess the secret word!
       </span>
     );
+  } else {
+    const guessedWordsRows = props.guessedWords.map(word => (
+      <tr data-test="guessed-word" key={ word.guessedWord }>
+        <td>{ word.guessedWord }</td>
+        <td>{ word.letterMatchCount }</td>
+      </tr>
+    ));
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr><th>Guess</th><th>Matching Letters</th></tr>
+          </thead>
+          <tbody>
+            { guessedWordsRows }
+          </tbody>
+        </table>
+      </div>
+    );
   }
   return (
     <div data-test="component-guessed-words">
--- jotto/src/GuessedWords.test.js
+++ jotto/src/GuessedWords.test.js
@@ -37,5 +37,25 @@ describe('if there are no words guessed', () => {
   });
 });
 describe('if there are words guessed', () => {
-
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 },
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test ('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+  test('correct number of guessed words', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNodes.length).toBe(guessedWords.length);
+  });
 });
