66b10c86d467ec73b70f724b6643ee032f78826b
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-05-01 12:09:44 -0700

    Complete Input component render tests for success=false

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -3,16 +3,34 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
+    const contents = this.props.success
+      ? null
+      : (
+        <form className="form-inline">
+          <input
+            data-test="input-box"
+            className="mb-2 mx-sm-3"
+            id="word-guess"
+            type="text"
+            placeholer="enter guess" />
+          <button
+            data-test="submit-button"
+            className="btn btn-primary mb-2"
+            type="submit">
+            Submit
+          </button>
+        </form>
+      );
     return (
-      <div>
-        <button />
+      <div data-test="component-input">
+        { contents }
       </div>
     )
   }
 };
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 }
 
 export default connect(mapStateToProps)(Input);
--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -13,18 +13,27 @@ import Input from './Input';
 const setup = (initialState={}) => {
   const store = storeFactory(initialState);
   const wrapper = shallow(<Input store={store} />).dive();
+  return wrapper;
 }
 
 describe('render', () => {
   describe('word has not been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    })
     test('renders component without error', () => {
-
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
     });
     test('renders input box', () => {
-
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(1);
     });
     test('renders submit button', () => {
-
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(1);
     });
   });
   describe('word has been guessed', () => {
