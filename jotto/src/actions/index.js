e23f49091ed0c64a831351abcacfa390b1711f9c
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-05-03 13:28:24 -0700

    Add secretWord action creator and reducer

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
 /**
@@ -28,3 +31,15 @@ export const guessWord = (guessedWord) => {
 
   };
 };
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    return axios.get('http://localhost:3030')
+      .then(response => {
+        dispatch({
+          type: actionTypes.SET_SECRET_WORD,
+          payload: response.data
+        });
+      });
+  }
+}
--- jotto/src/reducers/secretWordReducer.js
+++ jotto/src/reducers/secretWordReducer.js
@@ -1,3 +1,16 @@
+import { actionTypes } from '../actions';
+
+/**
+ * @function secretWordReducer
+ * @param {string} state - State before reducer.
+ * @param {object} action - Action sent to reducer.
+ * @returns {string} - New state (secret word payload from action).
+ */
 export default (state=null, action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.SET_SECRET_WORD:
+      return action.payload;
+    default:
+      return state;
+  }
 }
