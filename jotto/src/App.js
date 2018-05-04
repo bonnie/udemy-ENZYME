5da6360c6de197335dec32304a895295725ad38f
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-05-04 09:47:40 -0700

    Connect App component (and tests)

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import { getSecretWord } from './actions';
 
 class App extends Component {
   render() {
@@ -18,4 +20,9 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+}
+
+export default connect(mapStateToProps, { getSecretWord })(App);
