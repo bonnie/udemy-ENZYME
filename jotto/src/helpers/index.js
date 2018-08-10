8e357a92cbc81a08136ab6844c525f353e63961e
Andrew Clavin <andrewclavin8@gmail.com>
2018-08-09 22:49:12 -0400

    refactor(jotto helper functions): removing an unused set (#6)

--- jotto/src/helpers/index.js
+++ jotto/src/helpers/index.js
@@ -4,8 +4,8 @@
  * @param {string} secretWord - Secret word.
  * @returns {number} - Number of letters matched between guessed word and secret word.
  */
- export function getLetterMatchCount(guessedWord, secretWord) {
-   const secretLetterSet = new Set(secretWord.split(''));
-   const guessedLetterSet = new Set(guessedWord.split(''));
-   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length;
+export function getLetterMatchCount(guessedWord, secretWord) {
+    const secretLetters = secretWord.split('');
+    const guessedLetterSet = new Set(guessedWord.split(''));
+    return secretLetters.filter(letter => guessedLetterSet.has(letter)).length;
 };
