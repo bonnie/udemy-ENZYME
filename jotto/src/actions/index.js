c64acaa736fe8dad8c3eee4b29970f9f76a57ce9
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-08-27 21:21:49 -0700

    Add JSDoc for getSecretWord()

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -32,6 +32,11 @@ export const guessWord = (guessedWord) => {
   };
 };
 
+/**
+ * Returns Redux Thunk function that initiates an axios request 
+ *    and dispatches the response as a 'SET_SECRET_WORD' action
+ * @returns {function} - Redux Thunk function. 
+ */
 export const getSecretWord = () => {
   return (dispatch) => {
     return axios.get('http://localhost:3030')
