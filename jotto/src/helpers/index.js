3c33495e9ad2e604f150c96d435e3d4a43b88f8e
Darren <30411633+darrenrjones@users.noreply.github.com>
2018-12-11 20:49:58 -0800

    remove unnecessary split of 'guessedWord' (#9)
    const guessedLetterSet = new Set(guessedWord); 
    console logging guessedLetterSet returns the same as with the split('')

--- jotto/src/helpers/index.js
+++ jotto/src/helpers/index.js
@@ -6,6 +6,6 @@
  */
 export function getLetterMatchCount(guessedWord, secretWord) {
     const secretLetters = secretWord.split('');
-    const guessedLetterSet = new Set(guessedWord.split(''));
+    const guessedLetterSet = new Set(guessedWord);
     return secretLetters.filter(letter => guessedLetterSet.has(letter)).length;
 };
