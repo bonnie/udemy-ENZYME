26bc51f773a00a5c63c8f107e4258355cbc8e856
Bonnie Schulkin <biz@bonnieschulkin.com>
2020-02-22 20:35:02 -0800

    Add base project for starting hooks / context

---
+++ base-for-hooks-context/src/GuessedWords.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = (props) => {
+  let contents
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">
+        Try to guess the secret word!
+      </span>
+    );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <tr data-test="guessed-word" key={ index }>
+        <td>{ word.guessedWord }</td>
+        <td>{ word.letterMatchCount }</td>
+      </tr>
+    ));
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table className="table table-sm">
+          <thead className="thead-light">
+            <tr><th>Guess</th><th>Matching Letters</th></tr>
+          </thead>
+          <tbody>
+            { guessedWordsRows }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  return (
+    <div data-test="component-guessed-words">
+      { contents }
+    </div>
+  );
+};
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default GuessedWords;
---
+++ base-for-hooks-context/test/testUtils.js
@@ -0,0 +1,25 @@
+import checkPropTypes from 'check-prop-types';
+
+/**
+ * Return node(s) with the given data-test attribute.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} val - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+/**
+ * Throw error if conformingProps do not pass propTypes validation.
+ * @param {React.component} component - Component to check props against.
+ * @param {object} conformingProps - Props we expect to conform to defined propTypes.
+ */
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name);
+  expect(propError).toBeUndefined();
+}
