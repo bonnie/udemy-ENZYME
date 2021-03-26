bc6b04d150c505c2f3dc9aa1778cff0f4d5fef92
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-03-25 20:56:33 -0700

    Update mock for Redux return value

--- jotto-redux-hooks/src/actions/__mocks__/index.js
+++ jotto-redux-hooks/src/actions/__mocks__/index.js
@@ -1,6 +1,5 @@
 module.exports = {
-  ...jest.requireActual('..'),
+  ...jest.requireActual(".."),
   __esModule: true,
-  // TODO: update return value for Redux / context implementation
-  getSecretWord: jest.fn().mockReturnValue(Promise.resolve('party')),
-}
+  getSecretWord: jest.fn().mockReturnValue({ type: "mock" }),
+};
